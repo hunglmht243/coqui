8612c5a1111462bb52bba8a25001db6d555025be
hunglmht243 <dinhhung243.hd@gmail.com>
2023-03-24 14:28:06 +0700

    use_phoneme=false

--- TTS/tts/utils/text/vietnamese/phonemizer.py
+++ TTS/tts/utils/text/vietnamese/phonemizer.py
@@ -1,5 +1,8 @@
 from viphoneme import vi2IPA
+from unidecode import unidecode
 
 def vietnamese_text_to_phonemes(text: str) -> str:
+    text=unidecode(text)
     phoneme = vi2IPA(text)
+    
     return phoneme
